142bd2f02e4e2c35654109b5479f4f7c73180b21
Sven <nebel.sven@gmail.com>
2017-04-05 10:49:14 +0100

    Decouple to allow proper testing

--- game_of_life/game_of_life.py
+++ game_of_life/game_of_life.py
@@ -1,9 +1,7 @@
+# -*- coding: utf-8 -*-
 
-import sys
 import numpy
-import drawille
 
-from random import randrange
 from copy import deepcopy
 
 class GameOfLife(object):
@@ -65,38 +63,4 @@ class GameOfLife(object):
                         buffer_board[(i, j)] = 1
 
         self.board = buffer_board
-
-def main():
-    width, height = drawille.getTerminalSize()
-    game = GameOfLife(width=(int(width*2.0)-2), height=(int(height*4.0))-4)
-    
-    def fill_board_randomly(game):
-        def set_cell_randomly():
-            if randrange(10) > randrange(6, 10): return 1
-            return 0
-
-        for y in range(game.height):
-            for x in range(game.width):
-                game.board[y][x] = set_cell_randomly()
-        
-    fill_board_randomly(game)             
-    
-    def frame_coordinates(game):
-        while True:
-            game.evolve_board()
-            s = []
-            for y in range(game.height):
-                for x in range(game.width):
-                    if game.board[y][x] == 1: s.append((x, y))
-
-            yield s
-     
-    s = drawille.Canvas()
-    drawille.animate(s, frame_coordinates, 1./5, game)
-
-if __name__ == "__main__":
-    try:
-        main()
-    except KeyboardInterrupt:
-        sys.exit(0)
 
